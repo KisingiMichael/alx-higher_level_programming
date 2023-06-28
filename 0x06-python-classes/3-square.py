b374c5610e163873f0a7b81181255d6138e8c47d
KisingiMichael <kisingimichael12@gmail.com>
2023-06-27 20:03:04 -0700

    Fix more pycodestyle issues

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,11 @@
 
 
 class Square:
+    """Class constructor """
     def __init__(self, size=0):
+        """Initialize a square class
+        Ags: size=0: size of a the class square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -11,4 +15,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """compute area of class"""
         return self.__size ** 2
