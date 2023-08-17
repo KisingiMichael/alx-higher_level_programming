dcd57d4cb9cf41dad4da1f9891b8522946637384
KisingiMichael <kisingimichael12@gmail.com>
2023-08-17 16:32:29 -0700

    create database and table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+       	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL);
