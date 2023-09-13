212650555f8c086cb0abea890d7742f2a8adb02c
KisingiMichael <kisingimichael12@gmail.com>
2023-09-12 18:06:10 -0700

    Write a function that returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require('./5-square'){
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
