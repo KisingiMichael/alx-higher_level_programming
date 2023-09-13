a9e0566ca1a06aba960b5ed134343a09afd0df4e
KisingiMichael <kisingimichael12@gmail.com>
2023-09-12 17:39:45 -0700

    fix square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const Square = require('./5-square');
-module.exports = class Square extends Square{
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1{
   charPrint (c) {
     if (c === undefined) {
       this.print();
