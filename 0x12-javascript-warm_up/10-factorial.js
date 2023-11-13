fcce01813746ecafb462d3d1e9f705a5c4c4d547
KisingiMichael <kisingimichael12@gmail.com>
2023-11-13 15:27:41 -0800

    fix semistandard issues

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 function fact (n) {
-    return n === 0 || isNaN(n) ? 1 : n * fact(n - 1);
+  return n === 0 || isNaN(n) ? 1 : n * fact(n - 1);
 }
 console.log(fact(Number(process.argv[2])));
