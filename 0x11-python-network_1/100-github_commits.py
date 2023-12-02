2e2488e10a0281f30acc585b5a6336dae25a9ccc
KisingiMichael <kisingimichael12@gmail.com>
2023-12-01 18:54:12 -0800

    Display github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script taking 2 arguments to display github commits"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    m = requests.get(url)
+    commits = m.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
