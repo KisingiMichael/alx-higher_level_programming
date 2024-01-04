8c45f52c7c6de48aa63ca3506eb4aaa4d73594dd
KisingiMichael <kisingimichael12@gmail.com>
2024-01-03 17:27:27 -0800

    fix semistandard error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const request = require('request');
-const episode_num = process.argv[2];
+const episodeNum = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episode_num, function (err, response, body) {
+request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
